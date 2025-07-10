642231f74bee05cdfd4ab84dd7e9c61c455c6f55
pnoh <b1ueffrm@gmail.com>
2025-07-10 23:27:54 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250710/Date to Date/date-to-date.c
+++ 250710/Date to Date/date-to-date.c
@@ -5,22 +5,20 @@ int main(void) {
     scanf("%d %d %d %d", &m1, &d1, &m2, &d2);
     
     // Please write your code here.
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int from = 0;
     int to = 0;
-    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    while (m1 > 0)
+    for (int i = 0; i < m1; i++)
     {
-        from += num_of_days[m1];
-        m1--;
+        from += num_of_days[i];
     }
     from += d1;
-    while (m2 > 0)
+    for (int i = 0; i < m2; i++)
     {
-        to += num_of_days[m2];
-        m2--;
+        to += num_of_days[i];
     }
     to += d2;
     printf("%d\n", to - from + 1);
-    return 0;
+    return (0);
 }
+
