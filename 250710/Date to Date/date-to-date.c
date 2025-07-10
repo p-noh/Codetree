4089d071a03cdcbf9a1656edba83aebeb4b25a18
pnoh <b1ueffrm@gmail.com>
2025-07-10 23:05:52 +0900

    Title: Date to Date, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250710/Date to Date/date-to-date.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int m1, d1, m2, d2;
+    scanf("%d %d %d %d", &m1, &d1, &m2, &d2);
+    
+    // Please write your code here.
+    int from = 0;
+    int to = 0;
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    while (m1 > 0)
+    {
+        from += num_of_days[m1];
+        m1--;
+    }
+    from += d1;
+    while (m2 > 0)
+    {
+        to += num_of_days[m2];
+        m2--;
+    }
+    to += d2;
+    printf("%d\n", to - from);
+    return 0;
+}
