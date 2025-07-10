24e0eacf88b9b4350c7b08b25cabd00f80830de5
pnoh <b1ueffrm@gmail.com>
2025-07-10 23:43:56 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250710/요일 맞추기/guess-day-of-week.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int m1, d1, m2, d2;
+    scanf("%d %d %d %d", &m1, &d1, &m2, &d2);
+    
+    // Please write your code here.
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    char *day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    int from = 0;
+    int to = 0;
+    for(int i=0; i<m1; i++)
+        from += num_of_days[i];
+    from += d1;
+    for(int i=0; i<m2; i++)
+        to += num_of_days[i];
+    to += d2;
+    int ans = 0;
+    if (d2 > d1)
+        ans = (d2 - d1) % 7;
+    else
+        ans = 7 - ((d1 - d2) % 7);
+    printf("%s\n", day[ans]);
+    return 0;
+}
