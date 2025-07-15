390f6e8421882f477e9bea89e1434626ecf888b0
pnoh <b1ueffrm@gmail.com>
2025-07-15 21:36:57 +0900

    Title: 사각형의 총 넓이 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250715/사각형의 총 넓이 2/total-width-of-a-rectangle2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int n;
+int x1[10], y1[10], x2[10], y2[10];
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d %d %d", &x1[i], &y1[i], &x2[i], &y2[i]);
+    }
+    // Please write your code here.
+    return 0;
+}
